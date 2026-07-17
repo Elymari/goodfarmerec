d399ad5f079437c9b7c0cb66cea47c896081294b
Elymari <68340663+Elymari@users.noreply.github.com>
2026-07-17 09:10:33 -0500

    Update datos.js
    Added suppliers

--- datos.js
+++ datos.js
@@ -1,13 +1,24 @@
+//Aquí esta la lista de exportadoras junto al id del video en youtube para la parte de proceso correcto de empaque//
 const personas = [
 
 {
-nombre:"Ginafruit",
+nombre:"GINAFRUIT",
 youtube:"OSc0i90QAVM"
 },
 
 {
-nombre:"Mendoexport",
+nombre:"MENDOEXPORT",
 youtube:"2x30j1AioNU"
+},
+
+{
+nombre:"BAGATOCORP",
+youtube:"8wB4xK5bx3o"
+},
+
+{
+nombre:"NOBOA",
+youtube:"-8aVFYHmPGg"
 },
 
 ];
